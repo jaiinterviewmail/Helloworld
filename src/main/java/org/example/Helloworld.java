349f0424fdf911219c40bf204e92b2740b8b15fb
jaiinterviewmail <jaiinterviewmail@gmail.com>
2025-08-17 18:35:44 +0530

    second commint

--- src/main/java/org/example/Helloworld.java
+++ src/main/java/org/example/Helloworld.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world! my change");
+        System.out.println("Hello world! my change modified");
         System.out.println("This is a simple Java program.");
         System.out.println("It prints messages to the console.");
     }
