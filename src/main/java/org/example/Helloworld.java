5f8eaab1a6095bb7aa7bcea95589a2c917e4ce15
jaiinterviewmail <jaiinterviewmail@gmail.com>
2025-08-17 18:32:33 +0530

    first commint

---
+++ src/main/java/org/example/Helloworld.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints messages to the console.");
+    }
+}
+
---
+++ src/main/java/org/example/ThisDevFile.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ThisDevFile {
+    public static void main(String[] args) {
+        System.out.println("This is a development file.");
+
+    }
+
+}
